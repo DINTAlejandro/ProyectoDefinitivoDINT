a9dc95f6e6fefb9428d0f7b382d1b0a583366af1
dam1alejandro <90453213+dam1alejandro@users.noreply.github.com>
2023-01-30 13:57:43 +0100

    Realizado todo, falta implentar el servicio de mensajería, ya que no está realizado la vista con la lista de todos los autores y artículos.

--- Ventanas/EditarAutorVentana.xaml.cs
+++ Ventanas/EditarAutorVentana.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoDefinitivoDINT.VistasModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,12 @@ namespace ProyectoDefinitivoDINT.Ventanas
     /// </summary>
     public partial class EditarAutorVentana : Window
     {
+        EditarAutorVM vm;
         public EditarAutorVentana()
         {
             InitializeComponent();
+            vm = new EditarAutorVM();
+            this.DataContext = vm;
         }
     }
 }
---
+++ VistasModelo/EditarAutorVM.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDefinitivoDINT.VistasModelo
+{
+    class EditarAutorVM : ObservableRecipient
+    {
+        private Autor autorActual;
+        public Autor AutorActual
+        {
+            get { return autorActual; }
+            set { SetProperty(ref autorActual, value); }
+        }
+
+        public EditarAutorVM()
+        {
+
+        }
+    }
+}
