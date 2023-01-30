84e031e1baa0e7f7b5e81892a2b4c0ed97d71633
dam1alejandro <90453213+dam1alejandro@users.noreply.github.com>
2023-01-30 13:43:46 +0100

    Parte1

---
+++ Servicios/AbrirImagenesServicio.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDefinitivoDINT.Servicios
+{
+    class AbrirImagenesServicio
+    {
+        public AbrirImagenesServicio() { }
+
+        public string ObtenerImagen()
+        {
+            string ruta = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ruta = openFileDialog.FileName;
+            }
+            return ruta;
+        }
+    }
+}
