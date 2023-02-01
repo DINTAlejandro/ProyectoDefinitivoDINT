684fdfdbf4210ae8bec099daf4815574ef6f960c
JuanMateoSanchez <90640548+JuanMateoSanchez@users.noreply.github.com>
2023-02-01 12:52:55 +0100

    Corregido Using de autor

--- Mensajes/NuevaPersonaValueChangedMessage.cs
+++ Mensajes/NuevaPersonaValueChangedMessage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using ProyectoDefinitivoDINT.Clases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- VistasModelo/EditarAutorVM.cs
+++ VistasModelo/EditarAutorVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ProyectoDefinitivoDINT.Clases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
